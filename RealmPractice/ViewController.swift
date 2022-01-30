e0502330bcce170de669a5df6ff6d963b08b0eec
voitivit <kurbanov_emil@icloud.com>
2022-01-30 19:42:00 +0300

    FirstRealmFile

--- RealmPractice/ViewController.swift
+++ RealmPractice/ViewController.swift
@@ -6,14 +6,30 @@
 //
 
 import UIKit
+import RealmSwift
 
 class ViewController: UIViewController {
-
+    let info = Info()
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    getRealmAPI()
     }
-
+    func getRealmAPI() {
+        info.name = "Вероника"
+        info.surname = "Курбанова"
+        info.age = 11
+        info.gender = "Female"
+        do {
+           let realm = try Realm()
+            realm.beginWrite()
+            realm.add(info)
+            try realm.commitWrite()
+            print(realm.configuration.fileURL as Any)
+            
+        } catch  {
+            print(error)
+        }
+    }
 
 }
 
