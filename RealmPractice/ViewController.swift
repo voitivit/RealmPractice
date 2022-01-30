4b1ca7740ebe7e724d36d8deefd04f28fb91007a
voitivit <kurbanov_emil@icloud.com>
2022-01-30 18:57:53 +0300

    ProjectRealm

---
+++ RealmPractice/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RealmPractice
+//
+//  Created by emil kurbanov on 30.01.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
